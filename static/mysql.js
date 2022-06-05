cc95944f817185f75005e1117fe2a3eaa42dfe65
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-06-04 23:07:47 -0300

    Update mysql.js

--- static/mysql.js
+++ static/mysql.js
@@ -16,13 +16,13 @@ function add_ticket(person,description) {
   connection.query('INSER INTO ticket VALUES(${ticket_number},${person},${description});')
 }
 
-function search_ticket(number,person) {
+function search_ticket(number ,person) {
   if (number == 0) {
     connection.query('SELECT FROM ticket WHERE person = ${person}')
   } else if (person == null) {
     connection.query("SELECT FROM ticket WHERE person = ${number}");
   } else {
-    console.log('error, no information given')
+    // console.log('error, no information given')
   }
 }
 
