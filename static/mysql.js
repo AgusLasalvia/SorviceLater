9d3b786a3b397ed7fefcccc3e4f6583c36ffd291
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-06-04 22:26:02 -0300

    .

--- static/mysql.js
+++ static/mysql.js
@@ -1,15 +1,13 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'client',
-    password: '1234',
-    database:'Service'
-    
-})
-
+  host: "localhost",
+  user: "client",
+  password: "1234",
+  database: "Service",
+});
 
 connection.connect((err) => {
-    if (err) throw err
-    console.log('connection successful')
-})
+  if (err) throw err;
+  console.log("connection successful");
+});
