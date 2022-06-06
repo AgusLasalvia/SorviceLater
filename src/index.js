2e73a51090d1298571b548808a0ca5ad31b7d659
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-06-05 23:33:09 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const sql = require("mysql");
+const sql = require('mysql');
 
 //connection SQL
 sql.createConnection({
-  host: "localhost",
-  user: "admin",
-  password: "1234",
-  database: "tickets",
+  host: 'localhost',
+  user: 'admin',
+  password: '1234',
+  database: 'tickets'
 });
 
 app.listen(8080, () => {
-  console.log("Server start on port 8080");
+  console.log('Server start on port 8080');
 });
 
 //routes
-app.get("/", (recuest, response) => {
-  response.sendFile(path.join(__dirname, "/views/index.html"));
+app.get('/', (recuest, response) => {
+  response.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-app.get("/profile", (recuest, response) => {
-  response.sendFile(path.join(__dirname, "/views/profile.html"));
+app.get('/profile', (recuest, response) => {
+  response.sendFile(path.join(__dirname, '/views/profile.html'));
 });
