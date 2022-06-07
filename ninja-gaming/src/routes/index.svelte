e235e7066995afe6e1711b3f8085e92a77ebfb83
Ocaoj <joaquin.joaco.op7@gmail.com>
2022-06-07 19:46:18 -0300

    pelado

---
+++ ninja-gaming/src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+  let title = "Ninja Gaming Guides";
+
+  const updateTitle = () => {
+    title = "nigger pelado";
+  };
+</script>
+
+<div class="index">
+  <h2>{title}</h2>
+  <p>pelado peeeeeeeeeelaoo pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado pelado</p>
+
+  <button on:click={updateTitle}>cambiar titulong</button>
+  <input type="text" bind:value={title} />
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
