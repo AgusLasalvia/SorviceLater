74974420fa00378b0f5181dd39e9c48fab0dba52
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-06-11 19:33:07 -0300

    probando cosas

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function show() {
+    field_name = document.getElementById("email").value;
+    field_password = document.getElementById("password").value;
+    console.log(`name: ${field_name}, password: ${field_password}`)
+    
+}
