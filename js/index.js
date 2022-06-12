5201bb5dc39da13e15721ff632a95617788ec5b7
Ocaoj <joaquin.joaco.op7@gmail.com>
2022-06-12 19:12:44 -0300

    otro cosmitt
    asdas

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ function show() {
      field_name = document.getElementById("username").value;
      field_password = document.getElementById("password").value;
      console.log(`name: ${field_name}, password: ${field_password}`)
-
+     window.location.assign('../Templates/menu.html') // routes to menu.html
 }
