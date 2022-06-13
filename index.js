999c6dd44b02415708f4fe4102f39fb2da4bc869
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-06-13 19:38:54 -0300

    funca

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 //imports
-const express = require('express')
-const app = express()
-const path = require('path')
-const mysql = require('mysql')
+const express = require('express');
+const app = express();
+const path = require('path');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const port = 3000
 
 const connection = mysql.createConnection({
      host :"localhost",
      user: "root",
+     password: "",
+     database:"test"
+
 
 })
+var user = {}
+//app.use(express.urlencoded({ extended: false }));
 
 //statics
 app.use('/styles', express.static(__dirname + "/styles"))
 app.use('/js', express.static(__dirname + '/js'))
 app.use('/static', express.static(__dirname + '/static'))
 
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
 
 app.get('', (req, res) => {
      res.sendFile(path.join(__dirname, '/Templates/login.html'))
 })
-app.post('', async (req, res) => {
+app.post('/', (req, res) => {
      const { username, password } = req.body
-     
+     connection.query(`INSERT INTO person VALUES("${username}","${password}");`)
 })
 app.get('/menu', (req, res) => {
      res.sendFile(path.join(__dirname, '/Templates/menu.html'))
 })
 
 app.listen(port, () => console.info(`http://localhost:${port}`))
+
