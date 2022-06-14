06289136bae43301a74679215dde8be4e723fbd0
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-06-14 19:45:48 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ client = discord.Client()
 async def on_message(message):
     ticket_id = int(message.content)
     sql = list(cursor.execute(f'SELECT status, adittional_comments FROM ticket WHERE id = {ticket_id}'))
+    message.author.send(f'STATUS: {sql[0]},\n Comentaros: {sql[1]}')
 
 
 client.run('OTg2MTEwODY3MzY2MTE3Mzc4.G2eImO.C7A922zDyC0iVKH12olQjmBxyb1Lb198dJl5LU')
