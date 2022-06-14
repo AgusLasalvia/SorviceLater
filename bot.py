c09764c7876411fc69f16284aae9f04bbe408b51
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-06-14 12:36:56 -0300

    bot update

--- bot.py
+++ bot.py
@@ -1,32 +1,30 @@
+
+from config import token
 import discord
+from discord.ui import Button, View
 from discord.ext import commands
-#import pymysql
-
-TOKEN = 'OTg2MTEwODY3MzY2MTE3Mzc4.Ga4W0b.vxOhskEZwWM61SfOzIQuVoplRX9o8uBFAewWsk'
 
 
-# connection = pymysql.connect(
-#     host='localhost',
-#     user='root',
-#     passwd='',
-#     db=''
-# )
-# cursor = connection.cursor()
-client = discord.Client()
+bot = commands.Bot(command_prefix="!")
 
 
-@client.event
-async def on_message(message):
-    username = str(message.author).split('#')[0]
-    user_message = str(message.content)
-    if message.channel.name == 'servicelater':
-        if user_message.lower() == 'report':
-            user_message = ''
-            await message.channel.send('Ingresar nickname, problema')
-            #problem = str(message.content)
+@bot.command()
+async def report(ctx):
+    lost_item = Button(label="Perdida de item",
+                       style=discord.ButtonStyle.green)
+    hacker = Button(label="Hacker", style=discord.ButtonStyle.danger)
+    bug = Button(label="Bug", style=discord.ButtonStyle.blurple)
+    #button_4 = Button(label="Click me", style=discord.ButtonStyle.green)
+    view = View()
+    view.add_item(lost_item)
+    view.add_item(hacker)
+    view.add_item(bug)
+    await ctx.send("hi", view=view)
 
-client.run(TOKEN)
-
-def submit():
-    pass
-    
+# @client.event
+# async def on_message(message):
+#     username = str(message.author).split('#')[0]
+#     user_message = str(message.content)
+#     if message.channel.name == 'servicelater':
+#         if user_message.lower() == ''
+bot.run(token)
