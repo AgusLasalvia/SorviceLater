4477bbfe64d1e5529b298532a87774c878dc90ac
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-06-17 01:02:24 -0300

    creacion de los ejs

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ const bodyParser = require('body-parser');
 const port = 3000
 
 // database connection
-const connection = mysql.createConnection({
-     host: "localhost",
-     user: "admin",
-     password: "minecraft1234",
-     database: "servicelater"
-});
+// const connection = mysql.createConnection({
+//      host: "localhost",
+//      user: "admin",
+//      password: "minecraft1234",
+//      database: "servicelater"
+// });
 
-connection.connect((err) => {
-     if (err) throw err
-     console.log('db connected')
-});
+// connection.connect((err) => {
+//      if (err) throw err
+//      console.log('db connected')
+// });
 
 
 //statics
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
 app.use('/styles', express.static(__dirname + "/styles"));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/static', express.static(__dirname + '/static'));
@@ -30,35 +32,30 @@ app.use(bodyParser.json());
 
 
 //Routes
-app.get('/', (req, res) => {
-     res.sendFile(path.join(__dirname, '/Templates/login.html'))
-     const { } = req
+app.get('', (req, res) => {
+     res.render(path.join(__dirname, '/views/login'),)
 });
 
 
-app.post('/', (req, res) => {
+app.post('', (req, res) => {
      const { username, password } = req.body
      console.log(username, password)
-     connection.query(`SELECT username FROM Admin WHERE username = "${username}" AND password = "${password}"`, function (err, result, fields) {
-          if (err) throw err
-          console.log(result)
-          if (result[0].username === username) {
-               res.redirect(path.join(__dirname, './Templates/menu.html'))
-          } else {
-               res.send('<p>ERROR</p>')
-          }
+     // connection.query(`SELECT username FROM Admin WHERE username = "${username}" AND password = "${password}"`, function (err, result, fields) {
+     //      if (err) throw err
+     //      console.log(result)
+     res.render(path.join(__dirname, '/views/ticket'))
      })
-});
+// });
 
 
 
 app.get('/menu', (req, res) => {
-     res.sendFile(path.join(__dirname, '/Templates/menu.html'))
 });
 
 
 app.get('/ticket', (req, res) => {
-     res.sendFile(path.join(__dirname, './Templates/ticket.html'))
+     // res.sendFile(path.join(__dirname, './Templates/ticket.html'))
+     res.render(path.join(__dirname, '/views/ticket'),)
 });
 app.post('/ticket', (req, res) => {
 
