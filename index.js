5722e603f9bfda26b103f91d17afc3e201676054
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-06-19 01:52:25 -0300

    temporal ejs y update index.js
    Los archivos EJS se eliminaran y comenzara de 0 luego de terminado los HTML por completo

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('', function (req, res) {
      console.log(username, password)
      connection.query(`SELECT username FROM Admin WHERE username = "${username}" AND password = "${password}"`, function (err, result) {
           if (err) throw err
-          if (result[0] === username) {
+          if (result[0].username === username) {
                res.render(path.join(__dirname, '/views/menu'));
           }
      });
@@ -60,17 +60,17 @@ app.get('/menu', function (req, res) {
           inProgress: 0,
           ticketNew: 0
      }
-     connection.query('SELECT COUNT(*) FROM ticket WHERE status = "Resolved"', function (err, result) {
+     connection.query('SELECT COUNT(*) as Resolved FROM ticket WHERE status = "Resolved"', function (err, result) {
           if(err) throw err
-          data.totalResolved = result
+          data.totalResolved = result[0].Resolved
      });
-     connection.query('SELECT COUNT(*) FROM ticket WHERE status = "New"', function (err, result) {
+     connection.query('SELECT COUNT(*) as tkNew FROM ticket WHERE status = "New"', function (err, result) {
           if (err) throw err
-          data.ticketNew = result
+          data.ticketNew = result[0].tkNew
      });
-     connection.query('SELECT COUNT(*) FROM ticket WHERE status = "In Progress"', function (err, result) {
+     connection.query('SELECT COUNT(*) as inProgress FROM ticket WHERE status = "In Progress"', function (err, result) {
           if (err) throw err
-          data.inProgress = result
+          data.inProgress = result[0].inProgress
      });
      res.render(path.join(__dirname, '/views/menu'), { data: data })
 });
@@ -79,42 +79,23 @@ app.get('/menu', function (req, res) {
 
 //Tickets Route
 app.get('/ticket', function (req, res) {
-     const data = {
-          incNum: ticket_id,
-          reqBy: '',
-          reqFor: '',
-          srvcOf: '',
-          confItem: '',
-          contactType: '',
-          State: '',
-          Assigned: '',
-          Category: '',
-          Symptom: '',
-          Impact: '',
-          Urgency: '',
-          Priority: ''
-     } 
-     connection.query(`SELECT * FROM ticket id=${id}`, function (err, result) {
-          data.incNum = result[0], data.reqBy = result[1], data.reqFor = result[2]
-          data.srvcOf = result[3], data.confItem = result[4], data.contactType = result[5]
-          data.State = result[6], data.Assigned = result[7], data.Category = result[8]
-          data.Symptom = result[9], data.Impact = result[10], data.Urgency = result[11]
-          data.Priority = result[12]
+     connection.query(`SELECT * FROM Ticket WHERE id= 1;`, function (err, result) {
+          res.render(path.join(__dirname, '/views/ticket'), { data: result[0] });
      });
-     res.render(path.join(__dirname, '/views/ticket'), { data: data });
 });
+
 
 app.post('/ticket', function (req, res) {
      const { incNum, reqBy, reqFor, srvcOf,
           confItem, contactType, State,
           Assigned, Category, Symptom, Impact,
           Urgency, Priority } = req.body;
-     connection.query(`UPDATE Ticket SET request_by = "${reqBy}",\
-     request_for = "${reqFor}",service_offering = "${srvcOf}",\
-     item = "${confItem}",contact_type = "${contactType}",\
-     status = "${State}",assigned = "${Assigned}",\
-     category = "${Category}",symptom = "${Symptom}",\
-     impact = "${Impact}",urgency = "${Urgency}",priority = "${Priority}"\
+     connection.query(`UPDATE Ticket SET request_by = "${reqBy}",
+     request_for = "${reqFor}",service_offering = "${srvcOf}",
+     item = "${confItem}",contact_type = "${contactType}",
+     status = "${State}",assigned = "${Assigned}",
+     category = "${Category}",symptom = "${Symptom}",
+     impact = "${Impact}",urgency = "${Urgency}",priority = "${Priority}"
      WHERE id = ${incNum};`);
 });
 
@@ -122,8 +103,8 @@ app.post('/ticket', function (req, res) {
 //Ticket auto creator
 app.post('/ticket_create', function (req, res) {
      id_new = 0;
-     connection.query(`SELECT id FROM Ticket ORDER BY DESC;"`, function (err, result) {
-          id_new = result[0] + 1
+     connection.query(`SELECT COUNT(*) AS count FROM Ticket;"`, function (err, result) {
+          id_new = result[0].count + 1
      });
      connection.query(`INSERT INTO ticket VALUES(${id_new},NULL,\
      NULL,NULL,NULL,NULL,\
