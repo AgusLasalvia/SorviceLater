76799636ec4e8535a1036fb2713b638292dbd406
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-10-15 04:59:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ app.post('/', function (req, res) {
 
      connection.query(`SELECT * FROM admin WHERE username = '${username}' AND password = '${password}';`, function (err, result) {
           if (err) throw err
-          console.log(result.rows[0][username])
+          console.log(result.username)
           if (result.rows[0][username] == undefined) {
                res.render(path.join(__dirname, '/views/login'), { text: 'Username or password not correct' })
 
