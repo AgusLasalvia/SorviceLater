3360d9b019bcb1d299152796ffb9cd473359985c
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-10-14 23:07:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const port = 3000
 
 // Database connection
 const connection = mysql.createConnection({
-     host: "localhost",
-     user: "admin",
-     password: "minecraft1234",
-     database: "servicelater"
+     host: "ec2-44-209-24-62.compute-1.amazonaws.com",
+     user: "gnoellfbbbujkx",
+     password: "0fd585265a9e50e6a4965f9af22d5f18c49cf32dbda5ff0c29d437060cd4cd2d",
+     database: "da1eroecl12e1b",
+     port: 5432
 });
 
 connection.connect((err) => {
