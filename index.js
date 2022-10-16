3be6e2e4cc14511af56e68428c18295d06bb04a9
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-10-16 15:28:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -113,14 +113,14 @@ let search_kb = 0;
 //Important functions
 update_counters = () => {
      connection.query("SELECT COUNT(*) as count FROM ticket WHERE status = 'resolved';", function (err, resolved) {
-          console.log(resolved)
-          data.Resolve = resolved[0].count;
+          console.log(resolved.rows[0].count)
+          data.Resolve = resolved.rows[0].count;
 
           connection.query("SELECT COUNT(*) as count FROM ticket WHERE status = 'new';", function (err, t_new) {
-               data.New = t_new[0].count;
+               data.New = t_new.rows[0].count;
 
                connection.query("SELECT COUNT(*) as count FROM ticket WHERE status = 'pendingVendor' AND status = 'pendingAdmin';", function (err, progress) {
-                    data.Pending = progress[0].count
+                    data.Pending = progress.rows[0].count
                     
                });
           });
