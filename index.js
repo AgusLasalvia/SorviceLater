53ed781162b3a1526570841be612004e4360b300
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-10-16 00:33:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
 
-const { Pool, Client } = require('pg');
+const { Pool } = require('pg');
 
-// const pool = new Pool({
+// const connection = new Pool({
 //      connectionString: 'postgres://gnoellfbbbujkx:0fd585265a9e50e6a4965f9af22d5f18c49cf32dbda5ff0c29d437060cd4cd2d@ec2-44-209-24-62.compute-1.amazonaws.com:5432/da1eroecl12e1b',
 //      host: "ec2-44-209-24-62.compute-1.amazonaws.com",
 //      user: "gnoellfbbbujkx",
@@ -20,29 +20,16 @@ const { Pool, Client } = require('pg');
 //      }
 // });
 
-// pool.connect()
 
-const connection = new Client({
-     connectionString: 'postgres://gnoellfbbbujkx:0fd585265a9e50e6a4965f9af22d5f18c49cf32dbda5ff0c29d437060cd4cd2d@ec2-44-209-24-62.compute-1.amazonaws.com:5432/da1eroecl12e1b',
+// Database connection
+const connection = mysql.createConnection({
      host: "ec2-44-209-24-62.compute-1.amazonaws.com",
      user: "gnoellfbbbujkx",
      password: "0fd585265a9e50e6a4965f9af22d5f18c49cf32dbda5ff0c29d437060cd4cd2d",
      database: "da1eroecl12e1b",
      port: 5432,
-     ssl: {
-          rejectUnauthorized: false
-     }
+     hero
 });
-
-// Database connection
-// const connection = mysql.createConnection({
-//      host: "ec2-44-209-24-62.compute-1.amazonaws.com",
-//      user: "gnoellfbbbujkx",
-//      password: "0fd585265a9e50e6a4965f9af22d5f18c49cf32dbda5ff0c29d437060cd4cd2d",
-//      database: "da1eroecl12e1b",
-//      port: 5432,
-//      hero
-// });
 
 connection.connect((err) => {
      if (err) throw err
@@ -111,7 +98,7 @@ let search_kb = 0;
 
 //Important functions
 update_counters = () => {
-     connection.query("SELECT COUNT(*) as Count FROM ticket WHERE status = 'resolved';", function (err, resolved) {
+     connection.query("SELECT COUNT(*) as Count FROM ticket WHERE status = 'resolved';",function (err, resolved) {
           data.Resolve = resolved.rowCount;
 
           connection.query("SELECT COUNT(*) as count FROM ticket WHERE status = 'new';", function (err, t_new) {
