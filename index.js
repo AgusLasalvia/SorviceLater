b40c03b30e08816fe661e2c521bc9e44cc27a24e
Agustin Lasalvia <aguslblumenfeld@gmail.com>
2022-10-16 15:05:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ let search_kb = 0;
 
 //Important functions
 update_counters = () => {
-     connection.query("SELECT COUNT(*) as Count FROM ticket WHERE status = 'resolved';", function (err, resolved) {
+     connection.query("SELECT COUNT(*) as count FROM ticket WHERE status = 'resolved';", function (err, resolved) {
           data.Resolve = resolved[0].count;
 
           connection.query("SELECT COUNT(*) as count FROM ticket WHERE status = 'new';", function (err, t_new) {
