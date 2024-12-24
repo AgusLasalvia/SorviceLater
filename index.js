c9478c5dd3d759d5453b6a01bedf61d9a50051f6
AgusLasalvia <agus.blumenfeld13@gmail.com>
2024-12-23 23:46:37 -0300

    2024-2025 update init

--- index.js
+++ index.js
@@ -24,24 +24,25 @@ app.use(bodyParser.json());
 // express cookies and session configuration
 app.use(cookieParse())
 app.use(session({
-     secret: 'secret-key',
-     resave: false,
-     saveUninitialized: true
+	secret: 'secret-key',
+	resave: false,
+	saveUninitialized: true
 
 }));
 
 const user_data = {
-     username: '',
-     realname: '',
-     email: ''
+	username: '',
+	realname: '',
+	email: ''
 }
 //User data
-exports.user_data = user_data;  
+exports.user_data = user_data;
 
 // routes declaration
 const Login = require('./Routes/Login');
 const Backlog = require('./Routes/Backlog');
 const OpenTicket = require('./Routes/OpenTicket');
+
 const NewTicket = require('./Routes/NewTicket');
 const KnowledgeBase = require('./Routes/KnowledgeBase');
 const KnowleadgeCreate = require('./Routes/KnowledgeCreate');
@@ -56,64 +57,5 @@ app.use('/knowleadge_create', KnowleadgeCreate);
 app.use('/knowledgebase/:id', KnowledgeBase);
 app.use('/myInsidents', MyInsidents);
 
-
 //Server start url
 app.listen(port, () => console.info(`http://localhost:${port}`));
-
-
-
-// app.post('/search', (req, res) => {
-//      if (user_data.username === '') {
-//           res.redirect(path.join('/'))
-//      } else {
-//           const { Search } = req.body
-//           const [word, digit] = Search.split(/(?<=\D)(?=\d)/);
-
-//           switch (word) {
-//                case 'KB':
-//                     search_kb = parseInt(digit);
-//                     connection.query(`SELECT * FROM knowledgeBase WHERE KB = ${search_kb};`, function (err, first) {
-//                          res.render(path.join(__dirname, '/views/kbarticle'), { data: first.rows[0], user: user_data });
-//                     });
-//                     break;
-//                case 'INC':
-//                     search_ticket = parseInt(digit);
-//                     connection.query("SELECT COUNT(*) as count FROM KnowledgeBase;", function (err, first) {
-//                          connection.query("SELECT * FROM KnowledgeBase;", function (err, second) {
-//                               connection.query("SELECT COUNT(username) as count FROM admin;", function (err, third) {
-//                                    connection.query("SELECT username FROM admin;", function (err, forth) {
-//                                         connection.query(`SELECT * FROM ticket WHERE id = ${search_ticket};`, function (err, final) {
-//                                              res.render(path.join(__dirname, '/views/ticket'), {
-//                                                   data: final.rows[0], user: user_data,
-//                                                   KB: second.rows, count: first.rows[0].count,
-//                                                   user_count: third.rows[0].count, users: forth
-//                                              });
-//                                         });
-//                                    });
-//                               });
-
-//                          });
-//                     })
-//                     break;
-//           }
-//      }
-// })
-
-
-
-
-
-
-// app.get('/all_inc', function (req, res) {
-//      if (user_data.username === '') {
-//           res.redirect(path.join('/'))
-//      } else {
-//           connection.query("SELECT * FROM Ticket;", function (err, result) {
-//                connection.query("SELECT COUNT(*) AS count FROM Ticket;", function (err, first) {
-//                     res.render(path.join(__dirname, '/views/kblist'), {
-//                          title: 'Incidents', data: result.rows, count: first.rows[0], user: user_data
-//                     });
-//                });
-//           });
-//      }
-// });
--- sql.js
+++ sql.js
@@ -1,122 +1,122 @@
 const sql = require("mysql");
 const connection = sql.createConnection({
-    host: "<IP>",
-    user: "<USER>",
-    password: "<PASSWORD>",
-    database: "<DATABASE>"
+	host: "<IP>",
+	user: "<USER>",
+	password: "<PASSWORD>",
+	database: "<DATABASE>"
 });
 
 connection.connect(err => {
-    if (err) throw err;
-    console.log("Connected!");
+	if (err) throw err;
+	console.log("Connected!");
 });
 
 let data = {
-    Resolved: 0,
-    New: 0,
-    Pending: 0
+	Resolved: 0,
+	New: 0,
+	Pending: 0
 };
 
 function updateCounter() {
-    connection.query("SELECT COUNT(*) FROM ticket WHERE status = 'resolved';", function (err, resolved) {
-        if (err) throw err;
-        console.log(resolved.row[0].count);
-        data.Resolved = resolved.row[0].count;     
-    });
-    connection.query("SELECT COUNT(*) FROM ticket WHERE status = 'new';", function (err, newTicket) {
-        if (err) throw err;
-        console.log(newTicket.row[0].count);
-        data.New = newTicket.row[0].count;
-    });
-    connection.query("SELECT COUNT(*) FROM ticket WHERE status = 'pending';", function (err, pendingTicket) {
-        if (err) throw err;
-        console.log(pendingTicket.row[0].count);
-        data.Pending = pendingTicket.row[0].count;
-    });
-    return data;
+	connection.query("SELECT COUNT(*) FROM ticket WHERE status = 'resolved';", function (err, resolved) {
+		if (err) throw err;
+		console.log(resolved.row[0].count);
+		data.Resolved = resolved.row[0].count;
+	});
+	connection.query("SELECT COUNT(*) FROM ticket WHERE status = 'new';", function (err, newTicket) {
+		if (err) throw err;
+		console.log(newTicket.row[0].count);
+		data.New = newTicket.row[0].count;
+	});
+	connection.query("SELECT COUNT(*) FROM ticket WHERE status = 'pending';", function (err, pendingTicket) {
+		if (err) throw err;
+		console.log(pendingTicket.row[0].count);
+		data.Pending = pendingTicket.row[0].count;
+	});
+	return data;
 };
 
 function userVerification(username, password) {
-    connection.query(`SELECT * FROM admin WHERE username = '${username}' AND password = '${password}';`, function (err, response) {
-        if (err) throw err;
-        if (response.row[0].username === undefined) return false;
-        else return true;
-    })
+	connection.query(`SELECT * FROM admin WHERE username = '${username}' AND password = '${password}';`, function (err, response) {
+		if (err) throw err;
+		if (response.row[0].username === undefined) return false;
+		else return true;
+	})
 };
 
 
 function getAllAdmin() {
-    connection.query("SELECT username,COUNT(*) FROM admin;", function (err, admin) {
-        if (err) throw err;
-        console.log(admin);
-        return admin.rows
-    });
+	connection.query("SELECT username,COUNT(*) FROM admin;", function (err, admin) {
+		if (err) throw err;
+		console.log(admin);
+		return admin.rows
+	});
 };
 
 function getTicketCounter() {
-    connection.query("SELECT COUNT(*) FROM ticket;", function (err, ticket) {
-        if (err) throw err;
-        console.log(ticket.row[0].count);
-        return ticket.row[0].count
-    });
+	connection.query("SELECT COUNT(*) FROM ticket;", function (err, ticket) {
+		if (err) throw err;
+		console.log(ticket.row[0].count);
+		return ticket.row[0].count
+	});
 };
 
 
 function getAllKnowledgeBase() {
-    connection.query("SELECT *,COUNT(*) FROM knowledgebase;", function (err, knowledgebase) {
-        if (err) throw err;
-        console.log(knowledgebase);
-        return knowledgebase.rows
-    });
+	connection.query("SELECT *,COUNT(*) FROM knowledgebase;", function (err, knowledgebase) {
+		if (err) throw err;
+		console.log(knowledgebase);
+		return knowledgebase.rows
+	});
 };
 
 
-function getSpecificTicket(id) { 
-    connection.query(`SELECT * FROM ticket WHERE id = ${id};`, function (err, ticket) {
-        if (err) throw err;
-        console.log(ticket);
-        return ticket.rows
-    });
+function getSpecificTicket(id) {
+	connection.query(`SELECT * FROM ticket WHERE id = ${id};`, function (err, ticket) {
+		if (err) throw err;
+		console.log(ticket);
+		return ticket.rows
+	});
 };
 
 
 function insertNewTicket(ticket, user_data) {
-    const { incNum, reqBy, reqFor, srvcOf,
-        confItem, contactType, State,
-        Assigned, Category, Symptom, Impact,
-        Urgency, Description, Kb } = ticket;
-    const worknotes = ticket.worknotes
-    const addcomments = ticket.addcomments
-    let Priority = '';
-    switch (Impact) {
-        case 'high':
-            if (Urgency == 'high') {
+	const { incNum, reqBy, reqFor, srvcOf,
+		confItem, contactType, State,
+		Assigned, Category, Symptom, Impact,
+		Urgency, Description, Kb } = ticket;
+	const worknotes = ticket.worknotes
+	const addcomments = ticket.addcomments
+	let Priority = '';
+	switch (Impact) {
+		case 'high':
+			if (Urgency == 'high') {
 
-                Priority = '1-Urgent';
+				Priority = '1-Urgent';
 
-            } else {
-                Priority = '2-Very High';
+			} else {
+				Priority = '2-Very High';
 
-            }
-            break;
-        case 'medium':
-            if (Urgency == 'medium') {
-                Priority = '3-High';
+			}
+			break;
+		case 'medium':
+			if (Urgency == 'medium') {
+				Priority = '3-High';
 
-            } else {
-                Priority = '4-Medium';
+			} else {
+				Priority = '4-Medium';
 
-            }
-            break;
-        case 'low':
-            Urgency = 'low'
-            Priority = '5-Low';
-            break;
-    }
-    connection.query(`SELECT COUNT(id) as count FROM ticket WHERE id = ${incNum};`, function (err, result) {
-        if (err) throw err
-        if (result.rows[0].count == 1) {
-            connection.query(`UPDATE Ticket SET request_by = '${reqBy}',\
+			}
+			break;
+		case 'low':
+			Urgency = 'low'
+			Priority = '5-Low';
+			break;
+	}
+	connection.query(`SELECT COUNT(id) as count FROM ticket WHERE id = ${incNum};`, function (err, result) {
+		if (err) throw err
+		if (result.rows[0].count == 1) {
+			connection.query(`UPDATE Ticket SET request_by = '${reqBy}',\
           request_for = '${reqFor}',service_offering = '${srvcOf}',\
           item = '${confItem}',contact_type = '${contactType}',\
           status = '${State}',assigned = '${Assigned}',\
@@ -124,90 +124,90 @@ function insertNewTicket(ticket, user_data) {
           impact = "${Impact}",urgency = '${Urgency}',priority = '${Priority}',\
           description = '${Description}',KB = ${Kb},worknotes = '${worknotes}',\
           additional = '${addcomments}' WHERE id = ${incNum};`);
-            ;
-        } else {
-            connection.query(`INSERT INTO Ticket VALUES(${incNum},'${reqBy}',\
+			;
+		} else {
+			connection.query(`INSERT INTO Ticket VALUES(${incNum},'${reqBy}',\
           '${reqFor}','${srvcOf}','${confItem}','${contactType}',\
           '${State}','${Assigned}','${Category}','${Symptom}',\
           '${Impact}","${Urgency}','${Priority}','${Description}',${Kb},'${user_data.username}:  ${worknotes}',\
           '${user_data.username}:  ${addcomments}');`);
 
-        }
-    })
+		}
+	})
 };
 
 
 function getKnowledgeCounter() {
-    connection.query("SELECT COUNT(*) FROM knowledgebase;", function (err, knowledgebase) {
-        if (err) throw err;
-        console.log(knowledgebase.row[0].count);
-        return knowledgebase.row[0].count
-    });
+	connection.query("SELECT COUNT(*) FROM knowledgebase;", function (err, knowledgebase) {
+		if (err) throw err;
+		console.log(knowledgebase.row[0].count);
+		return knowledgebase.row[0].count
+	});
 };
 
 function getSpecificKnlowledgeBase(id) {
-    connection.query(`SELECT * FROM knowledgebase WHERE KB = ${id};`, function (err, knowledgebase) {
-        if (err) throw err;
-        console.log(knowledgebase);
-        return knowledgebase.rows[0]
-    });
+	connection.query(`SELECT * FROM knowledgebase WHERE KB = ${id};`, function (err, knowledgebase) {
+		if (err) throw err;
+		console.log(knowledgebase);
+		return knowledgebase.rows[0]
+	});
 };
 
 function getAllKnowledgeBase() {
-    connection.query("SELECT *,COUNT(*) FROM knowledgebase;", function (err, knowledgebase) {
-        if (err) throw err;
-        console.log(knowledgebase);
-        return knowledgebase.rows
-    });
+	connection.query("SELECT *,COUNT(*) FROM knowledgebase;", function (err, knowledgebase) {
+		if (err) throw err;
+		console.log(knowledgebase);
+		return knowledgebase.rows
+	});
 };
 
 function verifyExistingKnowledge(knowledge) {
-    connection.query(`SELECT COUNT(KB) as count FROM knowledgebase WHERE KB = ${knowledge};`, function (err, result) {
-        if (err) throw err
-        if (result.rows[0].count == 1) {
-            return true
-        } else {
-            return false
-        }
-    })
+	connection.query(`SELECT COUNT(KB) as count FROM knowledgebase WHERE KB = ${knowledge};`, function (err, result) {
+		if (err) throw err
+		if (result.rows[0].count == 1) {
+			return true
+		} else {
+			return false
+		}
+	})
 };
 
 
-function insertNewKnowledgeBase(knowledgebase, title, article) { 
-    connection.query(`INSERT INTO knowledgebase VALUES(${article},'${title}',\
-      '${knowledgebase}');`, function (err) { 
-        if (err) throw err;
-    });
+function insertNewKnowledgeBase(knowledgebase, title, article) {
+	connection.query(`INSERT INTO knowledgebase VALUES(${article},'${title}',\
+      '${knowledgebase}');`, function (err) {
+		if (err) throw err;
+	});
 };
 
-function updateKnowledgeBase(knowledgebase, title, article) { 
-    connection.query(`UPDATE knowledgebase SET title = '${title}',\
-      article = '${knowledgebase}' WHERE KB = ${article};`, function (err) { 
-        if (err) throw err;
-    });
+function updateKnowledgeBase(knowledgebase, title, article) {
+	connection.query(`UPDATE knowledgebase SET title = '${title}',\
+      article = '${knowledgebase}' WHERE KB = ${article};`, function (err) {
+		if (err) throw err;
+	});
 };
 
 
 function getAllMyIncidents(username) {
-    connection.query(`SELECT * FROM tickets WHERE username = '${username}';`, function (err,response) {
-        if (err) throw err;
-        return response.rows
-    })
+	connection.query(`SELECT * FROM tickets WHERE username = '${username}';`, function (err, response) {
+		if (err) throw err;
+		return response.rows
+	})
 };
 
 module.exports = {
-    getAllMyIncidents,
-    updateKnowledgeBase,
-    insertNewKnowledgeBase,
-    verifyExistingKnowledge,
-    getAllKnowledgeBase,
-    updateCounter,
-    userVerification,
-    getAllAdmin,
-    getTicketCounter,
-    getSpecificTicket,
-    insertNewTicket,
-    getKnowledgeCounter,
-    getSpecificKnlowledgeBase
+	getAllMyIncidents,
+	updateKnowledgeBase,
+	insertNewKnowledgeBase,
+	verifyExistingKnowledge,
+	getAllKnowledgeBase,
+	updateCounter,
+	userVerification,
+	getAllAdmin,
+	getTicketCounter,
+	getSpecificTicket,
+	insertNewTicket,
+	getKnowledgeCounter,
+	getSpecificKnlowledgeBase
 
 }
